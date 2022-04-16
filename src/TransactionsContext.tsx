3ebfb1d8df48a05e9e5a07e86c7bdbf38d0b53f8
Jonathan Thomsen <jthomsen.itj@gmail.com>
2022-04-15 23:13:00 -0300

    Finalizando inserção dos dados do formulário/modal e exibindo na lista

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -27,7 +27,7 @@ interface TransactionProviderProps {
 
 interface TransactionsContextData {
   transactions: Transaction[];
-  createTransaction: (transaction: TransactionInput) => void;
+  createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 
 //as = forçando o typescript a entender que tem tipagem e formato desejado
@@ -43,8 +43,14 @@ export function TransactionsProvider({ children }: TransactionProviderProps) {
       .then(response => setTransactions(response.data.transactions))
   }, []);
 
-  function createTransaction(transaction: TransactionInput) {
-    api.post('/transactions', transaction)
+  async function createTransaction(transactionInput: TransactionInput) {
+    const response = await api.post('/transactions', {
+      ...transactionInput,
+      createdAt: new Date()
+    })
+    const { transaction } = response.data;
+
+    setTransactions([...transactions, transaction]);
   }
 
   return (
