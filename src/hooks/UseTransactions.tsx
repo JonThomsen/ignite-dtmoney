c0a7c1d279df2abcf5a7bffd88d0f4b564ea521e
Jonathan Thomsen <jthomsen.itj@gmail.com>
2022-04-16 19:54:41 -0300

    Criando o hook useTransactions e ajustado os components

--- src/hooks/UseTransactions.tsx
+++ src/hooks/UseTransactions.tsx
@@ -1,5 +1,5 @@
-import { createContext, useEffect, useState, ReactNode } from 'react';
-import { api } from './services/api';
+import { createContext, useEffect, useState, ReactNode, useContext } from 'react';
+import { api } from '../services/api';
 
 interface Transaction {
   id: number;
@@ -31,7 +31,7 @@ interface TransactionsContextData {
 }
 
 //as = forçando o typescript a entender que tem tipagem e formato desejado
-export const TransactionsContext = createContext<TransactionsContextData>(
+const TransactionsContext = createContext<TransactionsContextData>(
   {} as TransactionsContextData
 );
 
@@ -58,4 +58,10 @@ export function TransactionsProvider({ children }: TransactionProviderProps) {
       {children}
     </TransactionsContext.Provider>
   );
+}
+
+export function useTransactions() {
+  const context = useContext(TransactionsContext);
+
+  return context;
 }
